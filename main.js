03f2f2f545aac290c3585af9d9669dbb019b7d52
ISTANBULBEKLE <ekipkalir@gmail.com>
2020-11-02 11:48:43 +0000

    lastDiv added, href added

--- main.js
+++ main.js
@@ -1,19 +1,32 @@
+/* 
+ The solution for this challenge requires JavaScript DOM manipulation. 
+ So we need;
+    1- to target second <article> element in the DOM, 
+    2- create a new <div> element, 
+    3- assign to this <div> the same class like the one in the siblings,
+    4- target the last <div> element in the second <article> and set its src attribute to www.google.com,
+    5- and than append new <div> element to its parent.
+    6- Lastly, we need to initialize this DOM event as a function and load with the onload of the window.  */
 
 
-let body = document.querySelector("body")
 
 function addDiv() {
-	let main = document.querySelector("main");
-	let articleElement = main.lastElementChild();
-	let newDiv = document.createElement("div");
-	newDiv.className = "square";
-	newDiv.appendChild(articleElement);
-    articleElement.appendChild(main);
-    main.appendChild(body);
+    let body = document.querySelector("body");
+    let main = document.querySelector("main");
+    let articleElement = document.querySelector("main").lastElementChild;
+    let newDiv = document.createElement("div");
+    newDiv.className = "square";
+    articleElement.appendChild(newDiv);
+    main.appendChild(articleElement);
+    body.appendChild(main);
 
-	let lastDiv = articleElement.lastChild();
-	lastDiv.setAttribute("src", "https://www.google.com/");
-	
-}
+    let lastDiv = document.querySelector(".square-container").lastElementChild;
+    lastDiv.setAttribute("href", "https://www.google.com/");
+    // let aTag = document.createElement("a");
+    // aTag.setAttribute("href", "https://www.google.com/");
+    // aTag.className = "square";
+    // lastDiv.appendChild(aTag);
+    console.log(lastDiv);
+};
 
 window.onload = addDiv;
